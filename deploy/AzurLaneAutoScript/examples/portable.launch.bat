278e3c936c5208e8a56a13ef0e48a0eb4c973f0d
6d6d <57275657+6d6d@users.noreply.github.com>
2024-06-08 15:11:53 +0800

    Update portable.launch.bat

--- deploy/AzurLaneAutoScript/examples/portable.launch.bat
+++ deploy/AzurLaneAutoScript/examples/portable.launch.bat
@@ -1,11 +1,7 @@
 @echo off
 echo %~dp0
-set "_root=%~dp0..\..\..\"
+set "_root=%~dp0..\..\..\..\"
 set "PATH=%_root%Python311;%_root%Python311\Scripts;%_root%Git\mingw64\bin;%PATH%"
 cd /d "%_root%FGO-py\FGO-py"
 python ../../deploy/updater.py
-if errorlevel 1 (
-    echo Update failed. See above.
-    exit
-)
-python fgo.py cli --no-color
+python fgo.py
